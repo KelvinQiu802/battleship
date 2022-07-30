f4530bfaa4cff1d9f9f69861d7310462a79eba6b
KelvinQiu <kelvinqiu802@outlook.com>
2022-07-30 14:38:03 +0800

    feat: 添加游戏状态

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
+import React from 'react';
+import * as GAME_STATE from './utils/gameState';
 
 const App = () => {
+  // 游戏状态，游戏阶段
+  const [gameState, setGameState] = React.useState(GAME_STATE.WELCOME);
+
   return (
     <div className='App'>
       <h1 className='title'>Battleship Project</h1>
