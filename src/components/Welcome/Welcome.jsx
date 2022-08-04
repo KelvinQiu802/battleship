7838c1b47fc2e3a76deff5d3c777570b6b99b58b
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-04 09:54:08 +0800

    fix: remove Bootstrap

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,6 @@
 import * as GAME_STATE from '../../utils/gameState';
 import React, { Component } from 'react';
+import Header from '../Header';
 
 const Welcome = ({ setGameState }) => {
   const handleStart = () => {
@@ -7,90 +8,87 @@ const Welcome = ({ setGameState }) => {
   };
 
   return (
-    <React.Fragment>
-      <div class='container'>
-        <header class='d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom'>
-          <a
-            href='/'
-            class='d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none'
-          >
-            <svg class='bi me-2' width='40' height='32'></svg>
-            <span class='fs-4'>Battleship</span>
-          </a>
-        </header>
-
-        <div class='px-4 pt-5 my-5 text-center'>
-          <h1 class='display-1 fw-bold'>欢迎游玩</h1>
-          <div class='col-lg-6 mx-auto'>
-            <p class='lead mb-4'>
+    <div className='welcome'>
+      <Header />
+      <div className='container'>
+        <div className='px-4 pt-5 my-5 text-center'>
+          <h1 className='display-1 fw-bold'>欢迎游玩</h1>
+          <div className='col-lg-6 mx-auto'>
+            <p className='lead mb-4'>
               海战（英语：Battleship，又名海战棋）是一款双人玩的猜谜游戏。
               {/* Battleship (also known as Battleships or Sea Battle) is a
-              strategytype guessing game for two players. */}
+              strategytype guessing game htmlFor two players. */}
             </p>
           </div>
         </div>
 
         <form>
-          <h1 class='h3 mb-3 fw-normal text-center'>请输入对战信息</h1>
+          <h1 className='h3 mb-3 fw-normal text-center'>请输入对战信息</h1>
 
-          <div class='input-group input-group-lg w-50 mx-auto'>
-            <span class='input-group-text' id='inputGroup-sizing-lg'>
+          <div className='input-group input-group-lg w-50 mx-auto'>
+            <span className='input-group-text' id='inputGroup-sizing-lg'>
               玩家1的昵称
             </span>
             <input
               type='text'
-              class='form-control'
+              className='htmlForm-control'
               aria-label='Sizing example input'
               aria-describedby='inputGroup-sizing-lg'
             />
           </div>
 
-          <div class='input-group input-group-lg w-50 mx-auto'>
-            <span class='input-group-text' id='inputGroup-sizing-lg'>
+          <div className='input-group input-group-lg w-50 mx-auto'>
+            <span className='input-group-text' id='inputGroup-sizing-lg'>
               玩家2的昵称
             </span>
             <input
               type='text'
-              class='form-control'
+              className='htmlForm-control'
               aria-label='Sizing example input'
               aria-describedby='inputGroup-sizing-lg'
             />
           </div>
 
-          <div class='text-center my-5'>
-            <div class='form-check form-check-inline '>
+          <div className='text-center my-5'>
+            <div className='htmlForm-check htmlForm-check-inline '>
               <input
-                class='form-check-input align-middle'
+                className='htmlForm-check-input align-middle'
                 type='radio'
                 name='inlineRadioOptions'
                 id='inlineRadio1'
                 value='option1'
               />
-              <label class='form-check-label fs-3' for='inlineRadio1'>
+              <label
+                className='htmlForm-check-label fs-3'
+                htmlFor='inlineRadio1'
+              >
                 单人模式
               </label>
             </div>
-            <div class='form-check form-check-inline'>
+            <div className='htmlForm-check htmlForm-check-inline'>
               <input
-                class='form-check-input'
+                className='htmlForm-check-input'
                 type='radio'
                 name='inlineRadioOptions'
                 id='inlineRadio2'
                 value='option2'
               />
-              <label className='form-check-label fs-3' for='inlineRadio2'>
+              <label
+                className='htmlForm-check-label fs-3'
+                htmlFor='inlineRadio2'
+              >
                 双人对战模式
               </label>
             </div>
           </div>
 
-          <div class='d-flex mb-5 mx-auto w-25'>
-            <span class='input-group-text ' id='inputGroup-sizing-lg'>
+          <div className='d-flex mb-5 mx-auto w-25'>
+            <span className='input-group-text ' id='inputGroup-sizing-lg'>
               选择语言
             </span>
             <select
-              class='form-select form-select-lg '
-              aria-label='.form-select-lg example'
+              className='htmlForm-select htmlForm-select-lg '
+              aria-label='.htmlForm-select-lg example'
             >
               <option selected>简体中文</option>
               <option value='1'>English</option>
@@ -98,13 +96,13 @@ const Welcome = ({ setGameState }) => {
           </div>
 
           <div className='text-center'>
-            <button class='w-50 btn btn-lg btn-primary' type='submit'>
+            <button className='w-50 btn btn-lg btn-primary' type='submit'>
               开始游戏
             </button>
           </div>
         </form>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
