cad68eb11c0cc70968473922cf59f6d20a4d62b9
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-04 13:22:00 +0800

    feat: 船基本信息

---
+++ src/utils/ships.js
@@ -0,0 +1,6 @@
+// 船基本属性
+export const CARRIER = { name: 'carrier', length: 5 };
+export const BATTLESHIP = { name: 'battleship', length: 4 };
+export const DESTROYER = { name: 'destroyer', length: 3 };
+export const SUBMARINE = { name: 'submarine', length: 3 };
+export const PATROL = { name: 'patrol', length: 2 };
