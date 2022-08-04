cb3dc945d1e221a6f047fa17e03433ba49a24378
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-04 13:42:31 +0800

    fix: 首字母大写

--- src/utils/ships.js
+++ src/utils/ships.js
@@ -1,6 +1,6 @@
 // 船基本属性
-export const CARRIER = { name: 'carrier', length: 5 };
-export const BATTLESHIP = { name: 'battleship', length: 4 };
-export const DESTROYER = { name: 'destroyer', length: 3 };
-export const SUBMARINE = { name: 'submarine', length: 3 };
-export const PATROL = { name: 'patrol', length: 2 };
+export const CARRIER = { name: 'Carrier', length: 5 };
+export const BATTLESHIP = { name: 'Battleship', length: 4 };
+export const DESTROYER = { name: 'Destroyer', length: 3 };
+export const SUBMARINE = { name: 'Submarine', length: 3 };
+export const PATROL = { name: 'Patrol', length: 2 };
