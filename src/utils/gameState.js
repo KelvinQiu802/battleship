529fb71e43a6282cc8e6e6bca2fd157d47cdae1c
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-06 11:37:28 +0800

    feaet: 电脑游戏状态

--- src/utils/gameState.js
+++ src/utils/gameState.js
@@ -1,9 +1,12 @@
 export const WELCOME = 'welcome';
 export const P1PLACING = 'p1placing';
 export const P2PLACING = 'p2placing';
+export const COMPUTER_PLACING = 'computerPlacing';
 export const READY = 'ready';
 export const START = 'start';
 export const P1ATTACK = 'p1attack';
 export const P2ATTACK = 'p2attack';
+export const COMPUTER_ATTACK = 'computerAttack';
 export const P1WIN = 'p1win';
 export const P2WIN = 'p2win';
+export const COMPUTER_WIN = 'computerWin';
