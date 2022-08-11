ec1f8caed16a460597f2d09a132420a685b2797d
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-11 16:36:30 +0800

    feat: 数据卡片

--- src/components/GameOver/DataCard.jsx
+++ src/components/GameOver/DataCard.jsx
@@ -1,17 +1,21 @@
 import * as BLOCK_STATE from '../../utils/blockStates';
+
 const DataCatd = ({ name, attack }) => {
   const numberOfAttack = attack.length - 1;
-  const numberOfHit = attack.filter(
-    (item) => item.state !== BLOCK_STATE.MISS
-  ).length;
+  const numberOfHit =
+    attack.filter((item) => item.state !== BLOCK_STATE.MISS).length - 1;
   const numberOfMiss = attack.filter(
     (item) => item.state === BLOCK_STATE.MISS
   ).length;
   const hitRate = (numberOfHit / numberOfAttack).toFixed(2) * 100;
 
   return (
-    <div>
-      {name}|{numberOfAttack}|{numberOfHit}|{numberOfMiss}|{hitRate}%
+    <div className='data-card'>
+      <h1 className='data-player'>{name}</h1>
+      <p>攻击次数: {numberOfAttack}</p>
+      <p>命中次数: {numberOfHit}</p>
+      <p>未命中次数: {numberOfMiss}</p>
+      <p>命中率: {hitRate}%</p>
     </div>
   );
 };
